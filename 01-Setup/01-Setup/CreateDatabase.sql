9bd3792c09defd91bc979e529e357e971f9224ba
Teddy2040-dot <Mandefroabuhay@gmail.com>
2026-08-05 23:50:09 -0400

    Add script to create AzurePerformanceLab database
    This script creates the AzurePerformanceLab database for the demo.

---
+++ 01-Setup/01-Setup/CreateDatabase.sql
@@ -0,0 +1,18 @@
+/*
+Project : Azure SQL Performance Tuning Lab
+File    : CreateDatabase.sql
+Purpose : Create the demo database (SQL Server example)
+
+Note:
+Azure SQL Database is normally created through the Azure Portal.
+This script is included for learning purposes.
+*/
+
+CREATE DATABASE AzurePerformanceLab;
+GO
+
+USE AzurePerformanceLab;
+GO
+
+PRINT 'AzurePerformanceLab database created successfully.';
+GO
